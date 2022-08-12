92b1be42fc90deb1d738fddbbf06a7b5648c411a
renjiedai <2312617455@qq.com>
2022-08-12 18:03:19 +0800

    评论按时间降序排序

--- pages/orderdishes/index.tsx
+++ pages/orderdishes/index.tsx
@@ -86,6 +86,8 @@ const InitialDish=(dishes):Array<DishProps>=>{
     dish["description"] = dishes.dish_all[i].dish_description;
     dish["dishtag"] = dishes.dish_all[i].dish_tag;
     dish["dishcomment"] = dishes.dish_all[i].dish_comment;
+    // 将评论按时间倒序排序
+    dish["dishcomment"].sort(function(a,b){return Date.parse(b.comment_time) - Date.parse(a.comment_time);});
     dish["video"] = "//player.bilibili.com/player.html?bvid="+dishes.dish_all[i].dish_video+"&page=1&high_quality=1&danmaku=0";
     // dish["dishcomment"] = [{content:"还挺好吃",time:"2022-08-06",stars:5},{content:"一般般",time:"2022-08-05",stars:4}];
 
